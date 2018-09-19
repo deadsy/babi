ef79df0cfa87448009c35feb09bcfa9406bddb2e
Jason Harris <sirmanlypowers@gmail.com>
2018-09-19 10:18:19 -0700

    Pushing for some more accuracy in the MinBLEP test vectors.

--- core/minblep/minblep.cpp
+++ core/minblep/minblep.cpp
@@ -5,330 +5,306 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <inttypes.h>
 
-#define PI 3.14159265358979f
+#define PI 3.14159265358979
 
 // SINC Function
-inline float SINC(float x)
-{
-  float pix;
+inline double SINC(double x) {
+	double pix;
 
-  if(x == 0.0f)
-    return 1.0f;
-  else
-  {
-    pix = PI * x;
-  return sinf(pix) / pix;
-  }
+	if (x == 0.0)
+		return 1.0;
+	else {
+		pix = PI * x;
+		return sin(pix) / pix;
+	}
 }
 
 // Generate Blackman Window
-inline void BlackmanWindow(int n, float *w)
-{
-  int m = n - 1;
-  int i;
-  float f1, f2, fm;
+inline void BlackmanWindow(int n, double *w) {
+	int m = n - 1;
+	int i;
+	double f1, f2, fm;
 
-  fm = (float)m;
-  for(i = 0; i <= m; i++)
-  {
-    f1 = (2.0f * PI * (float)i) / fm;
-    f2 = 2.0f * f1;
-    w[i] = 0.42f - (0.5f * cosf(f1)) + (0.08f * cosf(f2));
-  }
+	fm = (double)m;
+	for (i = 0; i <= m; i++) {
+		f1 = (2.0 * PI * (double)i) / fm;
+		f2 = 2.0 * f1;
+		w[i] = 0.42 - (0.5 * cos(f1)) + (0.08 * cos(f2));
+	}
 }
 
 // Discrete Fourier Transform
-void DFT(int n, float *realTime, float *imagTime, float *realFreq, float *imagFreq)
-{
-  int k, i;
-  float sr, si, p;
+void DFT(int n, double *realTime, double *imagTime, double *realFreq, double *imagFreq) {
+	int k, i;
+	double sr, si, p;
 
-  for(k = 0; k < n; k++)
-  {
-    realFreq[k] = 0.0f;
-    imagFreq[k] = 0.0f;
-  }
+	for (k = 0; k < n; k++) {
+		realFreq[k] = 0.0;
+		imagFreq[k] = 0.0;
+	}
 
-  for(k = 0; k < n; k++)
-    for(i = 0; i < n; i++)
-    {
-      p = (2.0f * PI * (float)(k * i)) / n;
-      sr = cosf(p);
-      si = -sinf(p);
-      realFreq[k] += (realTime[i] * sr) - (imagTime[i] * si);
-      imagFreq[k] += (realTime[i] * si) + (imagTime[i] * sr);
-    }
+	for (k = 0; k < n; k++)
+		for (i = 0; i < n; i++) {
+			p = (2.0 * PI * (double)(k * i)) / n;
+			sr = cos(p);
+			si = -sin(p);
+			realFreq[k] += (realTime[i] * sr) - (imagTime[i] * si);
+			imagFreq[k] += (realTime[i] * si) + (imagTime[i] * sr);
+		}
 }
 
 // Inverse Discrete Fourier Transform
-void InverseDFT(int n, float *realTime, float *imagTime, float *realFreq, float *imagFreq)
-{
-  int k, i;
-  float sr, si, p;
+void InverseDFT(int n, double *realTime, double *imagTime, double *realFreq, double *imagFreq) {
+	int k, i;
+	double sr, si, p;
 
-  for(k = 0; k < n; k++)
-  {
-    realTime[k] = 0.0f;
-    imagTime[k] = 0.0f;
-  }
+	for (k = 0; k < n; k++) {
+		realTime[k] = 0.0;
+		imagTime[k] = 0.0;
+	}
 
-  for(k = 0; k < n; k++)
-  {
-    for(i = 0; i < n; i++)
-    {
-      p = (2.0f * PI * (float)(k * i)) / n;
-      sr = cosf(p);
-      si = -sinf(p);
-      realTime[k] += (realFreq[i] * sr) + (imagFreq[i] * si);
-      imagTime[k] += (realFreq[i] * si) - (imagFreq[i] * sr);
-    }
-    realTime[k] /= n;
-    imagTime[k] /= n;
-  }
+	for (k = 0; k < n; k++) {
+		for (i = 0; i < n; i++) {
+			p = (2.0 * PI * (double)(k * i)) / n;
+			sr = cos(p);
+			si = -sin(p);
+			realTime[k] += (realFreq[i] * sr) + (imagFreq[i] * si);
+			imagTime[k] += (realFreq[i] * si) - (imagFreq[i] * sr);
+		}
+		realTime[k] /= n;
+		imagTime[k] /= n;
+	}
 }
 
 // Complex Absolute Value
-inline float cabs(float x, float y)
-{
-  return sqrtf((x * x) + (y * y));
+inline double cabs(double x, double y) {
+	return sqrt((x * x) + (y * y));
 }
 
 // Complex Exponential
-inline void cexp(float x, float y, float *zx, float *zy)
-{
-  float expx;
+inline void cexp(double x, double y, double *zx, double *zy) {
+	double expx;
 
-  expx = expf(x);
-  *zx = expx * cosf(y);
-  *zy = expx * sinf(y);
+	expx = exp(x);
+	*zx = expx * cos(y);
+	*zy = expx * sin(y);
 }
 
 // Compute Real Cepstrum Of Signal
-void RealCepstrum(int n, float *signal, float *realCepstrum)
-{
-  float *realTime, *imagTime, *realFreq, *imagFreq;
-  int i;
+void RealCepstrum(int n, double *signal, double *realCepstrum) {
+	double *realTime, *imagTime, *realFreq, *imagFreq;
+	int i;
 
-  realTime = new float[n];
-  imagTime = new float[n];
-  realFreq = new float[n];
-  imagFreq = new float[n];
+	realTime = new double[n];
+	imagTime = new double[n];
+	realFreq = new double[n];
+	imagFreq = new double[n];
 
-  // Compose Complex FFT Input
+	// Compose Complex FFT Input
 
-  for(i = 0; i < n; i++)
-  {
-    realTime[i] = signal[i];
-    imagTime[i] = 0.0f;
-  }
+	for (i = 0; i < n; i++) {
+		realTime[i] = signal[i];
+		imagTime[i] = 0.0;
+	}
 
-  // Perform DFT
+	// Perform DFT
 
-  DFT(n, realTime, imagTime, realFreq, imagFreq);
+	DFT(n, realTime, imagTime, realFreq, imagFreq);
 
-  // Calculate Log Of Absolute Value
+	// Calculate Log Of Absolute Value
 
-  for(i = 0; i < n; i++)
-  {
-    realFreq[i] = logf(cabs(realFreq[i], imagFreq[i]));
-    imagFreq[i] = 0.0f;
-  }
+	for (i = 0; i < n; i++) {
+		realFreq[i] = logf(cabs(realFreq[i], imagFreq[i]));
+		imagFreq[i] = 0.0;
+	}
 
-  // Perform Inverse FFT
+	// Perform Inverse FFT
 
-  InverseDFT(n, realTime, imagTime, realFreq, imagFreq);
+	InverseDFT(n, realTime, imagTime, realFreq, imagFreq);
 
-  // Output Real Part Of FFT
-  for(i = 0; i < n; i++)
-    realCepstrum[i] = realTime[i];
+	// Output Real Part Of FFT
+	for (i = 0; i < n; i++)
+		realCepstrum[i] = realTime[i];
 
-  delete realTime;
-  delete imagTime;
-  delete realFreq;
-  delete imagFreq;
+	delete realTime;
+	delete imagTime;
+	delete realFreq;
+	delete imagFreq;
 }
 
 // Compute Minimum Phase Reconstruction Of Signal
-void MinimumPhase(int n, float *realCepstrum, float *minimumPhase)
-{
-  int i, nd2;
-  float *realTime, *imagTime, *realFreq, *imagFreq;
+void MinimumPhase(int n, double *realCepstrum, double *minimumPhase) {
+	int i, nd2;
+	double *realTime, *imagTime, *realFreq, *imagFreq;
 
-  nd2 = n / 2;
-  realTime = new float[n];
-  imagTime = new float[n];
-  realFreq = new float[n];
-  imagFreq = new float[n];
+	nd2 = n / 2;
+	realTime = new double[n];
+	imagTime = new double[n];
+	realFreq = new double[n];
+	imagFreq = new double[n];
 
-  if((n % 2) == 1)
-  {
-    realTime[0] = realCepstrum[0];
-    for(i = 1; i < nd2; i++)
-      realTime[i] = 2.0f * realCepstrum[i];
-    for(i = nd2; i < n; i++)
-      realTime[i] = 0.0f;
-  }
-  else
-  {
-    realTime[0] = realCepstrum[0];
-    for(i = 1; i < nd2; i++)
-      realTime[i] = 2.0f * realCepstrum[i];
-    realTime[nd2] = realCepstrum[nd2];
-    for(i = nd2 + 1; i < n; i++)
-      realTime[i] = 0.0f;
-  }
+	if ((n % 2) == 1) {
+		realTime[0] = realCepstrum[0];
+		for (i = 1; i < nd2; i++)
+			realTime[i] = 2.0 * realCepstrum[i];
+		for (i = nd2; i < n; i++)
+			realTime[i] = 0.0;
+	} else {
+		realTime[0] = realCepstrum[0];
+		for (i = 1; i < nd2; i++)
+			realTime[i] = 2.0 * realCepstrum[i];
+		realTime[nd2] = realCepstrum[nd2];
+		for (i = nd2 + 1; i < n; i++)
+			realTime[i] = 0.0;
+	}
 
-  for(i = 0; i < n; i++)
-    imagTime[i] = 0.0f;
+	for (i = 0; i < n; i++)
+		imagTime[i] = 0.0;
 
-  DFT(n, realTime, imagTime, realFreq, imagFreq);
+	DFT(n, realTime, imagTime, realFreq, imagFreq);
 
-  for(i = 0; i < n; i++)
-    cexp(realFreq[i], imagFreq[i], &realFreq[i], &imagFreq[i]);
+	for (i = 0; i < n; i++)
+		cexp(realFreq[i], imagFreq[i], &realFreq[i], &imagFreq[i]);
 
-  InverseDFT(n, realTime, imagTime, realFreq, imagFreq);
+	InverseDFT(n, realTime, imagTime, realFreq, imagFreq);
 
-  for(i = 0; i < n; i++)
-    minimumPhase[i] = realTime[i];
+	for (i = 0; i < n; i++)
+		minimumPhase[i] = realTime[i];
 
-  delete realTime;
-  delete imagTime;
-  delete realFreq;
-  delete imagFreq;
+	delete realTime;
+	delete imagTime;
+	delete realFreq;
+	delete imagFreq;
 }
 
 // Generate MinBLEP And Return It In An Array Of Floating Point Values
-float *GenerateMinBLEP(int zeroCrossings, int overSampling)
-{
-  int i, n;
-  float r, a, b;
-  float *buffer1, *buffer2, *minBLEP;
+double *GenerateMinBLEP(int zeroCrossings, int overSampling) {
+	int i, n;
+	double r, a, b;
+	double *buffer1, *buffer2, *minBLEP;
 
-  n = (zeroCrossings * 2 * overSampling) + 1;
+	n = (zeroCrossings * 2 * overSampling) + 1;
 
-  buffer1 = new float[n];
-  buffer2 = new float[n];
+	buffer1 = new double[n];
+	buffer2 = new double[n];
 
-  // Generate Sinc
+	// Generate Sinc
 
-  a = (float)-zeroCrossings;
-  b = (float)zeroCrossings;
-  for(i = 0; i < n; i++)
-  {
-    r = ((float)i) / ((float)(n - 1));
-    buffer1[i] = SINC(a + (r * (b - a)));
-  }
+	a = (double)-zeroCrossings;
+	b = (double)zeroCrossings;
+	for (i = 0; i < n; i++) {
+		r = ((double)i) / ((double)(n - 1));
+		buffer1[i] = SINC(a + (r * (b - a)));
+	}
 
-  // Window Sinc
+	// Window Sinc
 
-  BlackmanWindow(n, buffer2);
-  for(i = 0; i < n; i++)
-    buffer1[i] *= buffer2[i];
+	BlackmanWindow(n, buffer2);
+	for (i = 0; i < n; i++)
+		buffer1[i] *= buffer2[i];
 
-  // Minimum Phase Reconstruction
+	// Minimum Phase Reconstruction
 
-  RealCepstrum(n, buffer1, buffer2);
-  MinimumPhase(n, buffer2, buffer1);
+	RealCepstrum(n, buffer1, buffer2);
+	MinimumPhase(n, buffer2, buffer1);
 
-  // Integrate Into MinBLEP
+	// Integrate Into MinBLEP
 
-  minBLEP = new float[n];
-  a = 0.0f;
-  for(i = 0; i < n; i++)
-  {
-    a += buffer1[i];
-    minBLEP[i] = a;
-  }
+	minBLEP = new double[n];
+	a = 0.0;
+	for (i = 0; i < n; i++) {
+		a += buffer1[i];
+		minBLEP[i] = a;
+	}
 
-  // Normalize
-  a = minBLEP[n - 1];
-  a = 1.0f / a;
-  for(i = 0; i < n; i++)
-    minBLEP[i] *= a;
+	// Normalize
+	a = minBLEP[n - 1];
+	a = 1.0 / a;
+	for (i = 0; i < n; i++)
+		minBLEP[i] *= a;
 
-  delete buffer1;
-  delete buffer2;
-  return minBLEP;
+	delete buffer1;
+	delete buffer2;
+	return minBLEP;
 }
 
-
-
-
-static void dump_buf(const char *str, float *buf, size_t n) {
-  if (str) {
-    printf("%s := []float64{\n", str);
-  }
-  for (int i = 0; i < n; i ++) {
-    printf("%.10f,", buf[i]);
-    if ((i != 0) && (i % 8 == 7)) {
-      printf("\n");
-    }
-  }
-  printf("}\n");
+static void dump_buf(const char *str, double *buf, size_t n) {
+	if (str) {
+		printf("%s := []uint64{\n", str);
+	}
+	for (int i = 0; i < n; i++) {
+		uint64_t *ptr = (uint64_t *) & buf[i];
+		//printf("%.10f,", buf[i]);
+		printf("0x%016lx,", *ptr);
+		if ((i != 0) && (i % 8 == 7)) {
+			printf("\n");
+		}
+	}
+	printf("}\n");
 }
 
-static float randMToN(float m, float n) {
-  float x = rand() / (RAND_MAX + 1.f);
-  return m + x * (n - m);
+static double randMToN(double m, double n) {
+	double x = rand() / (RAND_MAX + 1.f);
+	return m + x * (n - m);
 }
 
-static void rand_buf(float *buf, size_t n) {
-  for (int i = 0; i < n; i ++) {
-    buf[i] = randMToN(-10.f, 10.f);
-  }
+static void rand_buf(double *buf, size_t n) {
+	for (int i = 0; i < n; i++) {
+		buf[i] = randMToN(-10.0, 10.0);
+	}
 }
 
 #define WINDOW_SIZE 32
 
 int main(void) {
-  float w[WINDOW_SIZE];
-  float realTime[WINDOW_SIZE];
-  float imagTime[WINDOW_SIZE];
-  float realFreq[WINDOW_SIZE];
-  float imagFreq[WINDOW_SIZE];
-  float signal[WINDOW_SIZE];
-  float realCepstrum[WINDOW_SIZE];
-  float minimumPhase[WINDOW_SIZE];
+	double w[WINDOW_SIZE];
+	double realTime[WINDOW_SIZE];
+	double imagTime[WINDOW_SIZE];
+	double realFreq[WINDOW_SIZE];
+	double imagFreq[WINDOW_SIZE];
+	double signal[WINDOW_SIZE];
+	double realCepstrum[WINDOW_SIZE];
+	double minimumPhase[WINDOW_SIZE];
 
-  // blackman
-  BlackmanWindow(WINDOW_SIZE, w);
-  dump_buf("window", w, WINDOW_SIZE);
+	// blackman
+	BlackmanWindow(WINDOW_SIZE, w);
+	dump_buf("window", w, WINDOW_SIZE);
 
-  // dft
-  rand_buf(realTime, WINDOW_SIZE);
-  dump_buf("realTime", realTime, WINDOW_SIZE);
-  rand_buf(imagTime, WINDOW_SIZE);
-  dump_buf("imagTime", imagTime, WINDOW_SIZE);
-  DFT(WINDOW_SIZE, realTime, imagTime, realFreq, imagFreq);
-  dump_buf("realFreq", realFreq, WINDOW_SIZE);
-  dump_buf("imagFreq", imagFreq, WINDOW_SIZE);
+	// dft
+	rand_buf(realTime, WINDOW_SIZE);
+	dump_buf("realTime", realTime, WINDOW_SIZE);
+	rand_buf(imagTime, WINDOW_SIZE);
+	dump_buf("imagTime", imagTime, WINDOW_SIZE);
+	DFT(WINDOW_SIZE, realTime, imagTime, realFreq, imagFreq);
+	dump_buf("realFreq", realFreq, WINDOW_SIZE);
+	dump_buf("imagFreq", imagFreq, WINDOW_SIZE);
 
-  // idft
-  rand_buf(realFreq, WINDOW_SIZE);
-  dump_buf("realFreq", realFreq, WINDOW_SIZE);
-  rand_buf(imagFreq, WINDOW_SIZE);
-  dump_buf("imagFreq", imagFreq, WINDOW_SIZE);
-  InverseDFT(WINDOW_SIZE, realTime, imagTime, realFreq, imagFreq);
-  dump_buf("realTime", realTime, WINDOW_SIZE);
-  dump_buf("imagTime", imagTime, WINDOW_SIZE);
+	// idft
+	rand_buf(realFreq, WINDOW_SIZE);
+	dump_buf("realFreq", realFreq, WINDOW_SIZE);
+	rand_buf(imagFreq, WINDOW_SIZE);
+	dump_buf("imagFreq", imagFreq, WINDOW_SIZE);
+	InverseDFT(WINDOW_SIZE, realTime, imagTime, realFreq, imagFreq);
+	dump_buf("realTime", realTime, WINDOW_SIZE);
+	dump_buf("imagTime", imagTime, WINDOW_SIZE);
 
-  // real cepstrum
-  rand_buf(signal, WINDOW_SIZE);
-  RealCepstrum(WINDOW_SIZE, signal, realCepstrum);
-  dump_buf("signal", signal, WINDOW_SIZE);
-  dump_buf("realCepstrum", realCepstrum, WINDOW_SIZE);
+	// real cepstrum
+	rand_buf(signal, WINDOW_SIZE);
+	RealCepstrum(WINDOW_SIZE, signal, realCepstrum);
+	dump_buf("signal", signal, WINDOW_SIZE);
+	dump_buf("realCepstrum", realCepstrum, WINDOW_SIZE);
 
-  // minimum phase
-  rand_buf(realCepstrum, WINDOW_SIZE);
-  MinimumPhase(WINDOW_SIZE, realCepstrum, minimumPhase);
-  dump_buf("realCepstrum", realCepstrum, WINDOW_SIZE);
-  dump_buf("minimumPhase", minimumPhase, WINDOW_SIZE);
+	// minimum phase
+	rand_buf(realCepstrum, WINDOW_SIZE);
+	MinimumPhase(WINDOW_SIZE, realCepstrum, minimumPhase);
+	dump_buf("realCepstrum", realCepstrum, WINDOW_SIZE);
+	dump_buf("minimumPhase", minimumPhase, WINDOW_SIZE);
 
-  // MinBLEP
-  float *minBLEP = GenerateMinBLEP(6, 4);
-  dump_buf("minBLEP", minBLEP, 49);
+	// MinBLEP
+	double *minBLEP = GenerateMinBLEP(6, 4);
+	dump_buf("minBLEP", minBLEP, 49);
 
-
-  return 0;
+	return 0;
 }
