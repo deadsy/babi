4b9068acd6a285a3b25e7e45a73a8d105d5585e7
Jason Harris <sirmanlypowers@gmail.com>
2018-09-20 11:23:05 -0700

    More random number testing.

--- core/rand_test.go
+++ core/rand_test.go
@@ -15,16 +15,28 @@ import (
 //-----------------------------------------------------------------------------
 
 func TestFloat32(t *testing.T) {
+	buckets := make([]int, 10)
 	r := NewRand32(0)
-	for i := 0; i < 100; i++ {
-		t.Logf("%f", r.Float32())
+	for i := 0; i < 10000; i++ {
+		xf := (r.Float32() + 1.0) * 5.0
+		xi := int(xf)
+		buckets[xi]++
+	}
+	for i, v := range buckets {
+		t.Logf("%d: %d", i, v)
 	}
 }
 
 func TestFloat64(t *testing.T) {
+	buckets := make([]int, 10)
 	r := NewRand64(0)
-	for i := 0; i < 100; i++ {
-		t.Logf("%f", r.Float64())
+	for i := 0; i < 10000; i++ {
+		xf := (r.Float64() + 1.0) * 5.0
+		xi := int(xf)
+		buckets[xi]++
+	}
+	for i, v := range buckets {
+		t.Logf("%d: %d", i, v)
 	}
 }
 
